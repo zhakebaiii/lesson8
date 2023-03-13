adcf6ca44d8bfdb5810960e62a78bf08a06ec1fa
zhakebaiii <127180444+zhakebaiii@users.noreply.github.com>
2023-03-13 21:17:01 +0600

    Update init.sh

--- home/box/web/etc/init.sh
+++ home/box/web/etc/init.sh
@@ -1,5 +1,16 @@
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo pip3 install virtualenv
+sudo pip3 install pathlib2
+virtualenv -p python3 venv
+source venv/bin/activate
+pip3 install django
 
-sudo /etc/init.d/gunicorn restart
+sudo rm -rf/etc/nginx/sites-enabled/default
+
+sudo ln-sf/home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/nginx_2_1_11.conf
+
+sudo/etc/init.d/nginx restart
+
+gunicorn -w 2 -c /home/box/web/etc/hello.py hello:app &
+gunicorn -w 2 -c /home/box/web/etc/qa.py
+ask.wsgi:application & curl -vv
+'http://127.0.0.1:8000/login/'
